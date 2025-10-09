251cecc3c72c72ad1db8c4eb7a88dbfcc8949351
BrickmadeProductions <ryan.crowder6503@gmail.com>
2025-10-09 18:08:59 -0500

    Updated weaver to be ran by godot as a tool plugin.

--- plugin/ArcaneNetworkingPlugin.cs
+++ plugin/ArcaneNetworkingPlugin.cs
@@ -1,6 +1,8 @@
 #if TOOLS
 using Godot;
 using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace ArcaneNetworking;
 
@@ -20,6 +22,65 @@ public partial class ArcaneNetworkingPlugin : EditorPlugin
     {
         RemoveExportPlugin(_exportPlugin);
         GD.Print("[Arcane Networking] Weaver Plugin unloaded.");
+    }
+
+    public override bool _Build()
+    {
+        var projectName = ProjectSettings.GetSetting("application/config/name").AsStringName();
+
+        var targetDll = Path.Combine(".godot/mono/temp/bin/Debug/", $"{projectName}.dll");
+
+        return Weave(targetDll);
+    }
+
+    public static bool Weave(string dllPath)
+    {
+        GD.Print($"[Arcane Networking] Weave Begin.. {dllPath} ");
+
+        var projectDir = ProjectSettings.GlobalizePath("res://");
+        
+        if (!File.Exists(dllPath))
+        {
+            GD.PrintErr($"[Arcane Networking] Target DLL not found: {dllPath}");
+            return false;
+        }
+
+        var weaverDll = ProjectSettings.GlobalizePath("res://addons/arcane-networking/plugin/weaver/weaver.dll");
+
+        if (!File.Exists(weaverDll))
+        {
+            GD.PrintErr($"[Arcane Networking] Weaver not found: {weaverDll}");
+            return false;
+        }
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = $"\"{weaverDll}\" -- \"{dllPath}\"",
+            WorkingDirectory = projectDir,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
+
+        var proc = new Process { StartInfo = psi };
+        proc.Start();
+
+        string stdout = proc.StandardOutput.ReadToEnd();
+        string stderr = proc.StandardError.ReadToEnd();
+
+        proc.WaitForExit();
+
+        // if (!string.IsNullOrEmpty(stdout))
+        //     GD.Print("[Arcane Networking] \n" + stdout);
+
+        // if (!string.IsNullOrEmpty(stderr))
+        //     GD.PrintErr("[Arcane Networking] \n" + stderr);
+
+        GD.Print($"[Arcane Networking] Weaver finished with code {proc.ExitCode}");
+
+        return proc.ExitCode == 0;
     }
 
 }
